469e84f92c639e58a94b3434ca730745f89b63da
yiming543 <yiming543@yahoo.com.tw>
2025-06-24 13:21:50 +0800

    V03
    //20250624 V03 cs:2556
    //建立CRC8 table,簡化程式碼,省記憶空間.
    //RAM 420 Byte ==> 108 Byte
    //ROM 4491 Byte ==> 461 Byte

---
+++ TOYOTA_TX_V3.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=TOYOTA_TX_V3.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/TOYOTA_TX_V3.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=toyotatxv3.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/toyotatxv3.x.tar
